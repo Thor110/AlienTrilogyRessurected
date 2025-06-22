2a3c0b887f2efd45d4fb7ef752b4e19d1bfbe8e0
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-06-22 08:08:44 +0100

    notes and blank forms

--- ALTViewer/ALTViewer.Designer.cs
+++ ALTViewer/ALTViewer.Designer.cs
@@ -29,23 +29,83 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(332, 195);
+            button1.Location = new Point(12, 12);
             button1.Name = "button1";
-            button1.Size = new Size(83, 23);
+            button1.Size = new Size(99, 23);
             button1.TabIndex = 0;
             button1.Text = "Text Editor";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 41);
+            button2.Name = "button2";
+            button2.Size = new Size(99, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Enemy Viewer";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 70);
+            button3.Name = "button3";
+            button3.Size = new Size(99, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Graphics Viewer";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(12, 99);
+            button4.Name = "button4";
+            button4.Size = new Size(99, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Sound Effects";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(12, 128);
+            button5.Name = "button5";
+            button5.Size = new Size(99, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Palette Viewer";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(12, 157);
+            button6.Name = "button6";
+            button6.Size = new Size(99, 23);
+            button6.TabIndex = 5;
+            button6.Text = "Level Viewer";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // ALTViewer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "ALTViewer";
             Text = "ALTViewer";
@@ -55,5 +115,10 @@
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
     }
 }
---
+++ ALTViewer/EnemyViewer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ALTViewer
+{
+    public partial class EnemyViewer : Form
+    {
+        public EnemyViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
