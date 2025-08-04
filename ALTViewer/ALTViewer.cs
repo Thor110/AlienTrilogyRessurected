5672eee045b1c54f96f33d06eba27f46f371501b
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-08-04 07:04:34 +0100

    comment and patch notes

--- ALTViewer/ALTViewer.cs
+++ ALTViewer/ALTViewer.cs
@@ -52,10 +52,10 @@ namespace ALTViewer
             0x41C60L, 0x41C38L,  0x41C10L, 0x41BE8L, 0x41BC0L, 0x41B98L,                                        // fix 5 ( bridge section 2 )
             0x3D00CL, 0x3CA94L, 0x36E64L, 0x36068L,                                                             // fix 5 ( bridge section 3 )
             0x35F00L, 0x36CC0L, 0x37B5CL, 0x38908L, 0x396A0L, 0x3A474L, 0x3AD34L, 0x3C044L, 0x3C8F0L, 0x3CEA4L, // fix 5 ( bridge section 4 )
-            0x35C44L, 0x35C6CL, 0x35C94L, 0x35CBCL, 0x35CE4L, 0x35D0CL,                                               // fix 5 ( bridge section 5 )
-            0x31BD0L, 0x31BA8L, 0x31B80L, 0x31B58L, 0x31B30L, 0x31B08L,                                               // fix 5 ( bridge section 6 )
-            0x3C620L, 0x3CDF0L, 0x3D1ECL, 0x3D5E8L, 0x3DB38L, 0x3E164L,                                               // fix 5 ( bridge section 7 )
-            0x3E1DCL, 0x3DBB0L, 0x3D660L, 0x3D264L, 0x3CE68L, 0x3C698L };                                             // fix 5 ( bridge section 8 )
+            0x35C44L, 0x35C6CL, 0x35C94L, 0x35CBCL, 0x35CE4L, 0x35D0CL,                                         // fix 5 ( bridge section 5 )
+            0x31BD0L, 0x31BA8L, 0x31B80L, 0x31B58L, 0x31B30L, 0x31B08L,                                         // fix 5 ( bridge section 6 )
+            0x3C620L, 0x3CDF0L, 0x3D1ECL, 0x3D5E8L, 0x3DB38L, 0x3E164L,                                         // fix 5 ( bridge section 7 )
+            0x3E1DCL, 0x3DBB0L, 0x3D660L, 0x3D264L, 0x3CE68L, 0x3C698L };                                       // fix 5 ( bridge section 8 )
             foreach (long value in bridge) { BinaryUtility.ReplaceByte(value, 0x75, patchDirectory); }          // fix 5 ( bridge section )
             // fix texture flips
             long[] flip02 = { 0x3A476L, 0x35D36L, 0x35E26L, 0x35D86L, 0x35DD6L, 0x35E76L, 0x35EC6L, 0x2549AL,
